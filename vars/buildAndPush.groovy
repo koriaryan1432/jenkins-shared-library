0776da5c7bab87ab6953b4f6ba0740bf689de8a5
koriaryan1432 <koriaryan@gmail.com>
2026-06-16 01:12:05 +0530

    feat: use docker pull and --cache-from with latest tag to enable build caching in CI/CD pipeline

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -5,17 +5,22 @@
         // - buildContext: e.g., './backend'                                                                                                    
         // - buildTag: e.g., env.BUILD_NUMBER                                                                                                   
                                                                                                                                                 
-        String fullImageName = "${config.imageName}:${config.buildTag}"                                                                             
-                                                                                                                                                
-        echo "📦 Building Docker image: ${fullImageName}..."                                                                                    
-        if (isUnix()) {                                                                                                                         
-            sh "docker build -t ${fullImageName} ${config.buildContext}"                                                                        
-            echo "🚀 Pushing image to registry..."                                                                                              
-            sh "docker push ${fullImageName}"                                                                                                   
-        } else {                                                                                                                                
-            bat "docker build -t ${fullImageName} ${config.buildContext}"                                                                       
-            echo "🚀 Pushing image to registry..."                                                                                              
-            bat "docker push ${fullImageName}"                                                                                                  
+        String latestImageName = "${config.imageName}:latest"
+        String fullImageName = "${config.imageName}:${config.buildTag}"
+
+        echo "📦 Building Docker image: ${fullImageName}..."
+        if (isUnix()) {
+            sh "docker pull ${latestImageName} || true"
+            sh "docker build --cache-from ${latestImageName} -t ${fullImageName} -t ${latestImageName} ${config.buildContext}"
+            echo "🚀 Pushing images to registry..."
+            sh "docker push ${fullImageName}"
+            sh "docker push ${latestImageName}"
+        } else {
+            bat "docker pull ${latestImageName} || true"
+            bat "docker build --cache-from ${latestImageName} -t ${fullImageName} -t ${latestImageName} ${config.buildContext}"
+            echo "🚀 Pushing images to registry..."
+            bat "docker push ${fullImageName}"
+            bat "docker push ${latestImageName}"
         }                                                                                                                                       
         echo "✅ Image successfully published!"                                                                                                 
     }                
